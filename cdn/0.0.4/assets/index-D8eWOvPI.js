ece54c346437b94769a884cc26530ce5953e0715
Nils-Øyvind Lien <nils.lien@envo.no>
2026-01-09 11:59:42 +0100

    release: 0.0.4

---
+++ cdn/0.0.4/assets/index-D8eWOvPI.js
@@ -0,0 +1,1 @@
+import{b3 as o,P as a,b4 as r}from"./main-Cq4cLMRe.js";function b(){r({variableName:a("scrollbar.width").name})}function c(){o({variableName:a("scrollbar.width").name})}export{b,c as u};
