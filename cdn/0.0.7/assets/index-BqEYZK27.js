6971f6d287957ff9d1849bdbc1843726fdda0dc3
Nils-Øyvind Lien <nils.lien@envo.no>
2026-01-09 13:34:48 +0100

    release: 0.0.7

---
+++ cdn/0.0.7/assets/index-BqEYZK27.js
@@ -0,0 +1,1 @@
+import{b3 as o,P as a,b4 as r}from"./main-s9JDsInk.js";function b(){r({variableName:a("scrollbar.width").name})}function c(){o({variableName:a("scrollbar.width").name})}export{b,c as u};
